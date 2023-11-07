6219149f9c765a777dacd0911aea4a4afc66ebab
kmsoln <kmsoln@vk.com>
2023-11-07 22:27:16 +0300

    Added the Login logic inside " AccountController.cs " that uses " IdentityFramework " to achieve the goal.

--- Web Application/Controllers/AccountController.cs
+++ Web Application/Controllers/AccountController.cs
@@ -1,14 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Web_Application.Models;
 
 namespace Web_Application.Controllers;
 
 public class AccountController : Controller
 {
-    public AccountController() { }
+    
+    private readonly SignInManager<IdentityUser> _signInManager;
+    
+    public AccountController(SignInManager<IdentityUser> signInManager)
+    {
+        _signInManager = signInManager;
+    }
 
     public IActionResult Login()
     {
         return View();
     }
     
+    [HttpPost]
+    [AllowAnonymous]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                // User can log in, perform any desired actions
+                // For example, set authentication cookies and redirect
+                // to a specific page or the returnUrl
+
+                // For example, redirect to the Home page
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // User cannot log in, display an error message and stay on the login page
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            }
+        }
+        return View(model);
+    }
 }
