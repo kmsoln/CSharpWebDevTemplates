b5b5a4ae9346d50d39208298c53967911edc2c5b
kmsoln <kmsoln@vk.com>
2023-11-07 21:54:15 +0300

    Created the class controller that responsible to show razor pages

---
+++ Web Application/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Application.Controllers;
+
+public class AccountController : Controller
+{
+    public AccountController() { }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
