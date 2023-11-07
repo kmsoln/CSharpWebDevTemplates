fc87ebc063d9c177a2eb7594e76188e8361de877
kmsoln <kmsoln@vk.com>
2023-11-07 21:17:57 +0300

    Created Class " My Db Context" that inherits " IdentityDbContext" that uses the built-in class "IdentityUser" which is all are a part of Microsoft Identity Framework

---
+++ Web Application/Data/MyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web_Application.Data;
+
+public class MyDbContext : IdentityDbContext<IdentityUser>
+{
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+}
