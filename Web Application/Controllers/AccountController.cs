69bb0f36d7ccba434e20ef8f456c6e19d22ea5a1
kmsoln <kmsoln@vk.com>
2023-11-08 21:48:12 +0300

    Added the method of changing mail in "AccountController".

--- Web Application/Controllers/AccountController.cs
+++ Web Application/Controllers/AccountController.cs
@@ -152,4 +152,49 @@ public class AccountController : Controller
             return View(model);
         }
     }
+    
+    [Authorize]
+    [HttpGet]
+    public IActionResult ChangeMail()
+    {
+        // Implement logic to display the ChangeMail view
+        return View();
+    }
+
+    [Authorize]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> ChangeMail(ChangeMailViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user != null)
+            {
+                user.Email = model.NewEmail;
+                var result = await _userManager.UpdateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    // Email change successful, redirect to a success page or another action
+                    return RedirectToAction("Profile");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            else
+            {
+                // Handle the case where the user cannot be found
+                ModelState.AddModelError(string.Empty, "User not found.");
+            }
+        }
+
+        return View(model);
+    }
 }
