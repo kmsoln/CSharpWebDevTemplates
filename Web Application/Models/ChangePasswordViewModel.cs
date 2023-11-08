9a732501e7fa0ff91d2758f29b6b53b8b61c1938
kmsoln <kmsoln@vk.com>
2023-11-08 19:46:37 +0300

    Created the Change Password Model with it validations " ChangePasswordViewModel.cs "

---
+++ Web Application/Models/ChangePasswordViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Web_Application.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+public class ChangePasswordViewModel
+{
+    [Required(ErrorMessage = "Old Password is required")]
+    [DataType(DataType.Password)]
+    public string OldPassword { get; set; }
+
+    [Required(ErrorMessage = "New Password is required")]
+    [DataType(DataType.Password)]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).+$", ErrorMessage = "Password must meet complexity requirements")]
+    public string NewPassword { get; set; }
+
+    [Required(ErrorMessage = "Confirm Password is required")]
+    [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; }
+}
