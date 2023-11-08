7a3a53bccb9736329d9d5bacf831c43bef847349
kmsoln <kmsoln@vk.com>
2023-11-08 21:46:44 +0300

    Created "ChangeMailViewModel".

---
+++ Web Application/Models/ChangeMailViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_Application.Models;
+
+public class ChangeMailViewModel
+{
+    [Required(ErrorMessage = "Current Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
+    public string CurrentEmail { get; set; }
+
+    [Required(ErrorMessage = "New Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
+    public string NewEmail { get; set; }
+
+    [Compare("NewEmail", ErrorMessage = "Emails do not match")]
+    public string ConfirmNewEmail { get; set; }
+}
