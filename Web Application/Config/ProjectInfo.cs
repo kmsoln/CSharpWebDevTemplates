8a51f5d8a84d51f7a0b6b1a569aa162e3a7b5b81
kmsoln <kmsoln@vk.com>
2023-12-11 00:37:37 +0300

    Created ProjectInfo class that defines the main strings for the project.

---
+++ Web Application/Config/ProjectInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Web_Application.Config;
+
+public static class ProjectInfo
+{
+    public static readonly string ProjectName = "MyProject";
+    public static readonly string StudentName = "Karim Soliman";
+    public static readonly string StudentGroup = "PRI-121";
+}
