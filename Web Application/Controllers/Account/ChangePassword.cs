58936b05a4a2212d1d427a94e2899b9fae669451
kmsoln <kmsoln@vk.com>
2023-12-10 15:11:24 +0300

    Code improvement

---
+++ Web Application/Controllers/Account/ChangePassword.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Web_Application.Models;
+
+namespace Web_Application.Controllers.Account;
+
+public partial class AccountController
+{
+    
+    [HttpGet]
+    [Authorize]
+    public IActionResult ChangePassword()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [Authorize]
+    public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return RedirectToAction("Login");
+        }
+
+        var changePasswordResult = await _userManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+
+        if (changePasswordResult.Succeeded)
+        {
+            // Password changed successfully, you can redirect to a success page or perform other actions
+            return RedirectToAction("Profile", "Account");
+        }
+        else
+        {
+            // Password change failed, add errors to the ModelState
+            foreach (var error in changePasswordResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return View(model);
+        }
+    }
+    
+}
---
+++ Web Application/Controllers/Account/Profile.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Web_Application.Models;
+
+namespace Web_Application.Controllers.Account;
+
+public partial class AccountController
+{
+    [Authorize]
+    public IActionResult Profile()
+    {
+        var user = _userManager.GetUserAsync(User).Result;
+
+        var profileViewModel = new ProfileViewModel
+        {
+            UserName = user.UserName,
+            Email = user.Email,
+            // Populate other profile properties as needed
+        };
+
+        return View(profileViewModel);
+    }
+}
