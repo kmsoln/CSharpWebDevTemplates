f7cde17326276039fdc171fcf340139fbd1e466c
kmsoln <kmsoln@vk.com>
2023-12-17 21:20:37 +0300

    Added logging to ChangeMail.cs

--- Web Application/Controllers/Account/ChangeMail.cs
+++ Web Application/Controllers/Account/ChangeMail.cs
@@ -6,48 +6,60 @@ namespace Web_Application.Controllers.Account;
 
 public partial class AccountController
 {
+    
     [Authorize]
     [HttpGet]
     public IActionResult ChangeMail()
     {
-        // Implement logic to display the ChangeMail view
+        _logger.LogInformation("ChangeMail GET action called.");
         return View();
     }
-
+    
     [Authorize]
     [HttpPost]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> ChangeMail(ChangeMailViewModel model)
     {
+        _logger.LogInformation("ChangeMail POST action called.");
+        
         if (ModelState.IsValid)
         {
             var user = await _userManager.GetUserAsync(User);
-
+            
             if (user != null)
             {
-                user.Email = model.NewEmail;
-                var result = await _userManager.UpdateAsync(user);
-
-                if (result.Succeeded)
+                try
                 {
-                    // Email change successful, redirect to a success page or another action
-                    return RedirectToAction("Profile");
-                }
-                else
-                {
-                    foreach (var error in result.Errors)
+                    user.Email = model.NewEmail;
+                    var result = await _userManager.UpdateAsync(user);
+                    
+                    if (result.Succeeded)
                     {
-                        ModelState.AddModelError(string.Empty, error.Description);
+                        _logger.LogInformation($"Email change successful for user {user.UserName}.");
+                        return RedirectToAction("Profile");
                     }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                            _logger.LogError($"Error updating user email: {error.Description}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"An error occurred while updating user email: {ex.Message}");
+                    // Handle other exception details if needed
                 }
             }
             else
             {
-                // Handle the case where the user cannot be found
                 ModelState.AddModelError(string.Empty, "User not found.");
+                _logger.LogError("User not found during email change.");
             }
         }
-
+        
         return View(model);
     }
 }
