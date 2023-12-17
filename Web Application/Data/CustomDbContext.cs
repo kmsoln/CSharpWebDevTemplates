f5e7c47428c42661eb03676cf64399522107eb78
kmsoln <kmsoln@vk.com>
2023-12-18 00:34:54 +0300

    Created Class for Custom DbContext

---
+++ Web Application/Data/CustomDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Web_Application.Data;
+
+public class CustomDbContext : DbContext
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure relationships, indexes, etc.
+        // This method is optional and can be used to further configure the database model.
+    }
+}
