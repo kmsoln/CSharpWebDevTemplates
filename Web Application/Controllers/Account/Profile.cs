ae65a807cf78aeaa9d6b501416ee025a6c4e3f75
kmsoln <kmsoln@vk.com>
2023-12-17 21:24:47 +0300

    Added Logging to Profile.cs

--- Web Application/Controllers/Account/Profile.cs
+++ Web Application/Controllers/Account/Profile.cs
@@ -7,18 +7,31 @@ namespace Web_Application.Controllers.Account;
 
 public partial class AccountController
 {
+    
     [Authorize]
     public IActionResult Profile()
     {
+        _logger.LogInformation("Profile GET action called.");
+        
         var user = _userManager.GetUserAsync(User).Result;
-
-        var profileViewModel = new ProfileViewModel
+        
+        if (user != null)
         {
-            UserName = user.UserName,
-            Email = user.Email,
-            // Populate other profile properties as needed
-        };
-
-        return View(profileViewModel);
+            var profileViewModel = new ProfileViewModel
+            {
+                UserName = user.UserName,
+                Email = user.Email,
+                // Populate other profile properties as needed
+            };
+            
+            _logger.LogInformation($"User profile retrieved successfully for user {user.UserName}.");
+            
+            return View(profileViewModel);
+        }
+        else
+        {
+            _logger.LogError("User not found in Profile action.");
+            return RedirectToAction("Error", "Home");
+        }
     }
 }
