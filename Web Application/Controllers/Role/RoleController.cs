fb64c677786efa9c2a8a5491703a0de77fca59c9
kmsoln <kmsoln@vk.com>
2023-12-24 15:46:44 +0300

    Created RoleController

---
+++ Web Application/Controllers/Role/RoleController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Application.Controllers.Role;
+
+public partial class RoleController : Controller
+{
+    
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public RoleController(RoleManager<IdentityRole> roleManager)
+    {
+        _roleManager = roleManager;
+    }
+}
