122b4addf541497e84cf8f33a5cd64f3c2dcdb3a
kmsoln <kmsoln@vk.com>
2023-12-24 19:49:32 +0300

    Created ModifyUser method to modify user account

---
+++ Web Application/Controllers/User/ModifyUser.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_Application.Models.User;
+
+namespace Web_Application.Controllers.User;
+
+public partial class UserController
+{
+    public IActionResult ModifyUser(string userId)
+    {
+        var user = _userManager.FindByIdAsync(userId).Result;
+
+        if (user == null)
+        {
+            // Handle user not found
+            return NotFound();
+        }
+
+        // Retrieve user roles
+        var userRoles = _userManager.GetRolesAsync(user).Result;
+        foreach (var roleName in userRoles)
+        {
+            Console.WriteLine(roleName);
+        }
+
+        var availableRoles = _roleManager.Roles.ToList();
+
+        // Pass user data and roles to the ModifyUser view
+        var model = new ModifyUserViewModel
+        {
+            UserId = user.Id,
+            UserName = user.UserName,
+            PhoneNumber = user.PhoneNumber,
+            Email = user.Email,
+            Roles = userRoles,
+            AvailableRoles = availableRoles
+        };
+
+        return View(model);
+    }
+}
