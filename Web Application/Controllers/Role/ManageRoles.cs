ce09508b4d481ce76d1ada996b684547128dc1ea
kmsoln <kmsoln@vk.com>
2023-12-24 15:48:46 +0300

    Created ManageRoles.cs inside RoleController

---
+++ Web Application/Controllers/Role/ManageRoles.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_Application.Models.Role;
+
+namespace Web_Application.Controllers.Role;
+
+public partial class RoleController
+{
+    public IActionResult ManageRoles()
+    {
+        // Retrieve roles from your data source (Identity Framework using RoleManager)
+        var roles = _roleManager.Roles.ToList();
+
+        // Map roles to RoleViewModels
+        var roleViewModels = roles.Select(role => new RoleViewModel
+        {
+            Id = role.Id,
+            RoleName = role.Name
+        });
+
+        return View(roleViewModels);
+    }
+}
