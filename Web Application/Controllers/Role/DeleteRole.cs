c0e5879e5821e3608c5347461c6983a87b911da0
kmsoln <kmsoln@vk.com>
2023-12-24 15:47:32 +0300

    Created "DeleteRole" inside RoleController

---
+++ Web Application/Controllers/Role/DeleteRole.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Application.Controllers.Role;
+
+public partial class RoleController 
+{
+    
+    [HttpPost]
+    public async Task<IActionResult> DeleteRole(string roleId)
+    {
+        var role = await _roleManager.FindByIdAsync(roleId);
+
+        if (role == null)
+        {
+            // Role not found
+            return NotFound("Role not exists");
+        }
+
+        var result = await _roleManager.DeleteAsync(role);
+
+        if (result.Succeeded)
+        {
+            // Role deletion successful, return no content
+            return RedirectToAction("ManageRoles");
+        }
+
+        // Role deletion failed, return errors
+        return BadRequest(result.Errors);
+        
+    }
+}
