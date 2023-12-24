7b91136fe34ace8a7f1a710e7bcc27d839e18075
kmsoln <kmsoln@vk.com>
2023-12-24 19:50:09 +0300

    FIxed ModifyUserViewModel.cs

---
+++ Web Application/Models/User/ModifyUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Web_Application.Models.User;
+
+public class ModifyUserViewModel
+{
+    public string? UserId { get; set; }
+    public string? UserName { get; set; }
+    public string? Email { get; set; }
+    public  string? PhoneNumber { get; set; }
+    public string? SelectedRole { get; set;  }
+    
+    public IList<IdentityRole>? AvailableRoles { get; set; }
+    public IList<string>? Roles { get; set; }
+}
