ead9cbf67414d5a13b7fa613978fda00e3521dd7
kmsoln <kmsoln@vk.com>
2023-12-24 19:50:35 +0300

    Created Method DeleteUser

---
+++ Web Application/Controllers/User/DeleteUser.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web_Application.Models.User;
+
+namespace Web_Application.Controllers.User;
+
+public partial class UserController
+{
+    
+    [HttpPost]
+    public async Task<IActionResult> DeleteUser(string userId)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var result = await _userManager.DeleteAsync(user);
+
+        if (result.Succeeded)
+        {
+            // Redirect to ManageUsers after successful deletion
+            return RedirectToAction("ManageUsers");
+        }
+
+        // Handle errors if the deletion fails
+        ModelState.AddModelError("", "Failed to delete the user.");
+        return View("ManageUsers");
+    }
+}
