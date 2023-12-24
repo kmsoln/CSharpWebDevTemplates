92ccf9c0b2868b19f6b128397dc31a7ed6e1959c
kmsoln <kmsoln@vk.com>
2023-12-24 15:47:59 +0300

    Created "Update Role" inside RoleController

---
+++ Web Application/Controllers/Role/UpdateRole.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_Application.Models.Role;
+
+namespace Web_Application.Controllers.Role;
+
+public partial class RoleController
+{
+    
+    [HttpPost]
+    public async Task<IActionResult> UpdateRole(string roleId, string newRoleName)
+    {
+        var role = await _roleManager.FindByIdAsync(roleId);
+
+        if (role == null)
+        {
+            // Role not found
+            return NotFound();
+        }
+
+        role.Name = newRoleName;
+
+        var result = await _roleManager.UpdateAsync(role);
+
+        if (result.Succeeded)
+        {
+            // Role update successful, return the updated role
+            return RedirectToAction("ManageRoles");
+        }
+
+        // Role update failed, return errors
+        return BadRequest(result.Errors);
+    }
+}
