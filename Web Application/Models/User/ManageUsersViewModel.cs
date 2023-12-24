610b37b23a81b645e8ac0559404ba0e5e96bd18b
kmsoln <kmsoln@vk.com>
2023-12-24 19:49:43 +0300

    Created Model ManageUsersViewModel

---
+++ Web Application/Models/User/ManageUsersViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Web_Application.Auth;
+
+namespace Web_Application.Models.User;
+
+public class ManageUsersViewModel
+{
+    public List<AppUser>? Users { get; set; }
+    public Dictionary<string, List<string>>? UserRoles { get; set; }
+}
