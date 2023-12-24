617c12079186c05e19f6af0258fc16f0221d3ab1
kmsoln <kmsoln@vk.com>
2023-12-24 15:47:15 +0300

    Created "CreateRole" method inside RoleController

---
+++ Web Application/Controllers/Role/CreateRole.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Web_Application.Models.Role;
+
+namespace Web_Application.Controllers.Role;
+
+public partial class RoleController 
+{
+    
+        [HttpPost]
+        // Endpoint to create a new role
+        public async Task<IActionResult> CreateRole(string roleName)
+        {
+            var newRole = new IdentityRole { Name = roleName };
+
+            var result = await _roleManager.CreateAsync(newRole);
+
+            if (result.Succeeded)
+            {
+                // Role creation successful, return the created role
+                return RedirectToAction("ManageRoles");
+            }
+
+            // Role creation failed, return errors
+            return BadRequest(result.Errors);
+        }
+}
