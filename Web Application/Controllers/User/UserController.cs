e8f70fc3cdc413dfab5823b3d883842dc88fd176
kmsoln <kmsoln@vk.com>
2023-12-24 19:46:37 +0300

    Created UserController

---
+++ Web Application/Controllers/User/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Web_Application.Auth;
+
+namespace Web_Application.Controllers.User;
+
+public partial class UserController : Controller
+{
+    
+    private readonly UserManager<AppUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly ILogger<UserController> _logger;
+
+    public UserController(
+        UserManager<AppUser> userManager, 
+        RoleManager<IdentityRole> roleManager, 
+        ILogger<UserController> logger)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _logger = logger;
+    }
+    
+}
