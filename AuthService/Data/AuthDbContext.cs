585f1742afe91355a1d07df47a317d33a679c281
kmsoln <kmsoln@vk.com>
2024-02-05 04:39:08 +0300

    updated Authentication Database Context

--- AuthService/Data/AuthDbContext.cs
+++ AuthService/Data/AuthDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AuthService.Auth;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuthService.Data;
 
