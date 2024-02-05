9e6a08f41a7317f1ff2afb7624254ec28ce7cca0
kmsoln <kmsoln@vk.com>
2024-02-05 04:34:19 +0300

    Created DbContext for authentication

---
+++ AuthService/Data/AuthDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Data;
+
+public class AuthDbContext : IdentityDbContext<AppUser>
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options)
+        : base(options)
+    {
+    }
+}
