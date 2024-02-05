40296578a29d4487021d04edad961964bc5a93e3
kmsoln <kmsoln@vk.com>
2024-02-05 04:23:21 +0300

    Moved from Minimal api approach to Controllers

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -13,5 +14,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+    
+app.MapControllers();
 
 app.Run();
