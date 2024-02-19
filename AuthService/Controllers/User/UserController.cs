6a51b6e0b885b07661838f2b9e4f8696dd88c9a6
kmsoln <kmsoln@vk.com>
2024-02-19 04:52:17 +0300

    Created UserController.cs

---
+++ AuthService/Controllers/User/UserController.cs
@@ -0,0 +1,10 @@
+﻿using AuthService.Auth;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Controllers.User;
+
+public partial class UserController(UserManager<AppUser> userManager,
+    RoleManager<IdentityRole> roleManager,
+    ILogger<UserController> logger
+) : Controller;
