00f6a1e7e4999cbc994660d186194421f02dd656
kmsoln <kmsoln@vk.com>
2024-02-19 03:28:57 +0300

    Enhancement on AccountController.cs

--- AuthService/Controllers/Account/AccountController.cs
+++ AuthService/Controllers/Account/AccountController.cs
@@ -7,7 +7,7 @@ namespace AuthService.Controllers.Account;
 [ApiController]
 [Route("[controller]")]
 public partial class AccountController(
-        UserManager<AppUser> userManager,
-        SignInManager<AppUser> signInManager,
-        ILogger<AccountController> logger
-        ) : Controller;
+    UserManager<AppUser> userManager,
+    SignInManager<AppUser> signInManager,
+    ILogger<AccountController> logger
+) : Controller;
