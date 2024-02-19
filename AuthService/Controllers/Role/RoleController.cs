1739f114089d2d2dadc382174b7ecf13b1c1f48d
kmsoln <kmsoln@vk.com>
2024-02-19 04:24:09 +0300

    Allow Role Controller only for authenticated user ( Admins only in disabled for testing reasons )

--- AuthService/Controllers/Role/RoleController.cs
+++ AuthService/Controllers/Role/RoleController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AuthService.Controllers.Role;
 
+[Authorize]
+// [Authorize(Roles = "admin")]
 public partial class RoleController(
     RoleManager<IdentityRole> roleManager,
     ILogger<RoleController> logger
