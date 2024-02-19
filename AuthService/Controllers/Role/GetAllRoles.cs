ef47f14dd53116ad327d275a473f5aa11726e869
kmsoln <kmsoln@vk.com>
2024-02-19 04:30:29 +0300

    Added GetAllRoles.cs

---
+++ AuthService/Controllers/Role/GetAllRoles.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Controllers.Role;
+
+public partial class RoleController
+{
+    [HttpGet("AllRoles")]
+    [Authorize]
+    public async Task<IActionResult> AllRoles()
+    {
+        try
+        {
+            Log(LogLevel.Information, "AllRoles GET action called.");
+
+            var allRoles = await roleManager.Roles.ToListAsync();
+            return Ok(allRoles);
+        }
+        catch (Exception ex)
+        {
+            Log(LogLevel.Error, $"An error occurred while retrieving all roles: {ex.Message}");
+            return StatusCode(500, "An error occurred while retrieving all roles.");
+        }
+    }
+}
