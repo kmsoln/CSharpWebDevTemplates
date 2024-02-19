44121dddf3844601160beb1652283be0db4b8b3f
kmsoln <kmsoln@vk.com>
2024-02-19 03:51:43 +0300

    Added Change Mail Model

---
+++ AuthService/Models/Account/ChangeMailModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthService.Models.Account;
+
+public class ChangeMailModel
+{
+    [Required(ErrorMessage = "New Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
+    public string? NewEmail { get; set; }
+
+    [Compare("NewEmail", ErrorMessage = "Emails do not match")]
+    public string? ConfirmNewEmail { get; set; }
+}
