c833c9123c8f9556e066bc6579f69c6ee3f599ff
kmsoln <kmsoln@vk.com>
2024-02-19 04:09:30 +0300

    Created RoleController.cs

---
+++ AuthService/Controllers/Role/RoleController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Controllers.Role;
+
+public partial class RoleController(
+    RoleManager<IdentityRole> roleManager,
+    ILogger<RoleController> logger
+) : Controller;
