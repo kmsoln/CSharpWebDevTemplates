b67424d271831b54e10772793568b3a71c57428d
kmsoln <kmsoln@vk.com>
2024-02-23 00:12:25 +0300

    Refactored project folder structure

--- Microservices/Common/obj/Debug/net8.0/Common.AssemblyInfo.cs
+++ Microservices/Common/obj/Debug/net8.0/Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca72d56349ed61706c8b251798a497184ec6adc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
