ac0c0e6821693dd85f6de9fd4ac15e1af4b2c753
kmsoln <kmsoln@vk.com>
2024-02-23 17:37:56 +0300

    Created the DbContext

---
+++ Microservices/InfoService/Data/CustomDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InfoService.Data;
+
+public class CustomDbContext : DbContext
+{
+    public CustomDbContext(DbContextOptions<CustomDbContext> options)
+        : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure relationships, indexes, etc.
+        // This method is optional and can be used to further configure the database model.
+    }
+}
