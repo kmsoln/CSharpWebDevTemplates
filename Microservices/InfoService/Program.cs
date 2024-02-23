c0395d6816c894ea8f6b242488d507b1c870b912
kmsoln <kmsoln@vk.com>
2024-02-23 17:30:23 +0300

    Create new project InfoService

---
+++ Microservices/InfoService/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+#region ConfigureServices
+// Configure services for the application
+builder.Services.AddControllers(); // Add MVC controllers
+builder.Services.AddEndpointsApiExplorer(); // Add API Explorer for endpoints
+builder.Services.AddSwaggerGen(); // Add Swagger for API documentation
+#endregion
+
+// Build the application
+var app = builder.Build();
+
+#region ConfigurePipeline
+// Configure the HTTP request pipeline.
+
+// If the application is in development mode
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(); // Enable Swagger middleware
+    app.UseSwaggerUI(); // Enable Swagger UI for interactive documentation
+}
+
+app.UseHttpsRedirection(); // Redirect HTTP to HTTPS
+
+app.MapControllers(); // Map controllers for handling requests
+
+#endregion
+
+// Run the application
+app.Run();
