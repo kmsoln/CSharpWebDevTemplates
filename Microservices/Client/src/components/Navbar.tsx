e201339dc892139e66d5da594086aadfa709cabb
kmsoln <kmsoln@vk.com>
2024-02-25 16:53:44 +0300

    Added AdminSection to navbar

--- Microservices/Client/src/components/Navbar.tsx
+++ Microservices/Client/src/components/Navbar.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import NavbarContainer from "./navbar/NavbarContainer";
+import NavbarItem from "./navbar/NavbarItem";
+import AdminSection from "./navbar/AdminSection";
+import AuthenticatedSection from "./navbar/AuthenticatedSection";
+import NonAuthenticatedSection from "./navbar/NonAuthenticatedSection";
+import useAuth from "../hooks/useAuth";
 
 
 const Navbar: React.FC = () => {
+    
+    const auth = useAuth()
+    const isAuthenticated = auth.isAuthenticated
+    const userName = auth.user?.userName
     
     return (
         <NavbarContainer>
@@ -14,6 +23,14 @@ const Navbar: React.FC = () => {
                         aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
+
+                {/* Navbar content with flex layout */}
+                <div className="navbar-collapse collapse d-sm-inline-flex justify-content-between">
+                    <ul className="navbar-nav flex-grow-1">
+                        <NavbarItem label={"Home"} url={"/"}/>
+                        {isAuthenticated ? <AdminSection/> : ("")}
+                    </ul>
+                </div>
             </div>
         </NavbarContainer>
     );
