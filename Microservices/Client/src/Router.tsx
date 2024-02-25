af131da11a93e96e9e37d3abc6b963358405ada6
kmsoln <kmsoln@vk.com>
2024-02-25 16:48:01 +0300

    removed wrong imports

--- Microservices/Client/src/Router.tsx
+++ Microservices/Client/src/Router.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import Home from "./pages/home/Home";
-import LoginForm from "./pages/account/LoginForm";
 
 function Router() {
     return (
